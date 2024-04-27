1dbb564cd4325ed8f6e8c3b11b1462509f042aed
High23 <warenasb2@gmail.com>
2024-04-26 20:03:55 -0700

    Initial commit

---
+++ routes/new.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router()
+const messages = require("../public/javascripts/messages")
+
+router.get("/", function(req, res, next) {
+    res.render('form');
+})
+
+router.post("/", function(req, res, next) {
+    const messageText = req.body.messageText;
+    const username = req.body.authorName;
+    messages.push({text: messageText, username: username, added: new Date()});
+    res.redirect('/');
+})
+
+module.exports = router
